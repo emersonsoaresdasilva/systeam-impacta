e31ea610c3c214f28305d98b5cd83b4d99080320
IgorShimun <igor.souza.teixeira18@gmail.com>
2020-11-10 19:48:28 -0300

    Revert "Formulários equipes/partidas"
    This reverts commit 5ff449e57e16a81749283662e7752b644abb6425.

--- database/funcoes.py
+++ database/funcoes.py
@@ -50,7 +50,3 @@ def listar_partidas_da_equipe(Equipe):
 ''' BANCO DADOS USUARIOS '''
 def pegar_usuario(email, senha):
     return [u for u in USUARIOS if u.email == u.email][0]
-
-
-def criar_equipe(Equipe):
-    EQUIPES.append(Equipe)
